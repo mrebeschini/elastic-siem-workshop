905fc4f0313c6140654211cfe708925270d4abec
Matteo Rebeschini <matteo@elastic.co>
2019-08-01 20:45:49 -0600

    Update beats_install.sh

--- beats_install.sh
+++ beats_install.sh
@@ -2,20 +2,24 @@
 # Setup beats on Linux                                                                                                                                                                                                                   
 CONFIG_REPOSITORY_URL="https://raw.githubusercontent.com/mrebeschini/2019BSidesLV/master/"                                                                                                                                               
 
+# Uninstall Beats if already installed
+sudo yum -y -q remove filebeat metricbeat packetbeat auditbeat 2>1 /dev/null
+
 CLOUD_ID=""
 CLOUD_AUTH=""
 echo "Enter your Elastic Cloud CLOUD_ID then press [ENTER]"
 read CLOUD_ID
-echo "Your CLOUD_ID is set to $CLOUD_ID\n\n"
+echo -e "Your CLOUD_ID is set to $CLOUD_ID\n\n"
 echo "Enter you Elastic Cloud 'elastic' user password and then press [ENTER]"
 read CLOUD_AUTH
-echo "Your elastic password is set to $CLOUD_AUTH\n\n"
+echo -e "Your elastic password is set to $CLOUD_AUTH\n\n"
 echo "Ready to Install? [y|n]"
 read CONTINUE
 case "$CONTINUE" in
- "y") echo "Elastic Beats Installation Initiated";;
- *) echo "Installation aborted";exit;;
+ [Yy]) echo "Elastic Beats Installation Initiated";;
+    *) echo "Installation aborted";exit;;
 esac
+
 # Download Elastic yum repo configuration.
 echo "Downloading Elastic yum repo configuration from github."
 sudo curl -O $CONFIG_REPOSITORY_URL/elastic-7.x.repo > /home/centos/elastic-7.x.repo
@@ -24,9 +28,7 @@ printf "\n\n\n"
 echo "Installing filebeat, packetbeat, metricbeat, and auditbeat rpms"
 sudo yum install filebeat packetbeat metricbeat auditbeat -y
 printf "\n\n\n"
-echo "Downloading beats configuration files"
-sleep 2
-printf "\n\n\n"
+echo "Downloading beats configuration files \n\n"
 sudo curl -O $CONFIG_REPOSITORY_URL/auditd-attack.rules.conf > ./attack.rules.conf
 sudo mv -v ./auditd-attack.rules.conf  /etc/auditbeat/audit.rules.d/auditd-attack.rules.conf
 sudo curl -O $CONFIG_REPOSITORY_URL/auditbeat.yml > ./auditbeat.yml
@@ -45,9 +47,8 @@ sudo curl -O $CONFIG_REPOSITORY_URL/packetbeat.yml > ./packetbeat.yml
 sudo mv -v ./packetbeat.yml  /etc/packetbeat/packetbeat.yml
 sudo chown root  /etc/packetbeat/packetbeat.yml
 sudo chmod go-w /etc/packetbeat/packetbeat.yml
-printf "\n\n\n"
+echo -e "\n\n\n"
 echo "Setting up keystore with Elastic Cloud credentials"
-sleep 5
 sudo auditbeat keystore create --force
 sudo filebeat keystore create --force
 sudo packetbeat keystore create --force
@@ -60,15 +61,12 @@ echo $CLOUD_ID|sudo packetbeat keystore add CLOUD_ID --stdin --force
 echo $CLOUD_AUTH|sudo packetbeat keystore add --stdin CLOUD_AUTH --force
 echo $CLOUD_ID|sudo metricbeat keystore add CLOUD_ID --stdin --force
 echo $CLOUD_AUTH|sudo metricbeat keystore add --stdin CLOUD_AUTH --force
-printf "\n\n\n"
-echo "Stopping auditd deamon"
+echo -e "\n\nStopping auditd deamon"
 sudo service auditd stop
-printf "\n\n\n"
-echo "Setting up auditbeat"
+echo -e "\n\nSetting up auditbeat"
 sudo auditbeat setup
 sudo systemctl start auditbeat
-printf "\n\n\n"
-echo "Setting up filebeat"
+echo -e "\n\nSetting up filebeat"
 sudo filebeat modules enable system
 sudo filebeat setup
 sudo systemctl start filebeat
@@ -76,16 +74,12 @@ printf "\n\n\n"
 echo "Setting up packetbeat"
 sudo packetbeat setup
 sudo systemctl start packetbeat
-printf "\n\n\n"
-echo "Setting up metricbeat"
+echo -e "\n\nSetting up metricbeat"
 sudo metricbeat setup
 sudo systemctl start metricbeat
-printf "\n\n\n"
-echo "Testing beats output"
+echo -e "\n\nTesting beats output"
 sudo auditbeat test output
 sudo metricbeat test output
 sudo filebeat test output
 sudo packetbeat test output
-printf "\n\n\n"
-sleep 5
-echo "Setup complete"
+echo -e "\n\nSetup complete"
