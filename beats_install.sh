4cd89711c12290ec72e11ae2e7448e1ffc917328
Matteo Rebeschini <matteo@elastic.co>
2019-08-26 17:45:54 -0600

    Update beats_install.sh

--- beats_install.sh
+++ beats_install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash                                                                                                                                                                                                                              
-# Setup beats on Linux                                                                                                                                                                                                                   
-CONFIG_REPOSITORY_URL="https://raw.githubusercontent.com/mrebeschini/2019BSidesLV/master"                                                                                                                                               
+CONFIG_REPOSITORY_URL="https://raw.githubusercontent.com/mrebeschini/elastic-siem-workshop/master" 
 
-# Uninstall Beats if already installed
-sudo yum -y -q remove filebeat metricbeat packetbeat auditbeat 2>1 /dev/null
+echo "Elastic Beats Installer"
+echo "-----------------------"
 
-CLOUD_ID=""
-CLOUD_AUTH=""
+if [[ $EUID -ne 0 ]]; then
+   echo "Error: this script must be run as root." 
+   exit 1
+fi
+
 echo "Enter your Elastic Cloud CLOUD_ID then press [ENTER]"
 read CLOUD_ID
-echo -e "Your CLOUD_ID is set to $CLOUD_ID\n\n"
+if [ -z "$CLOUD_ID" ]; then
+    echo "Error: CLOUD_ID must be set to a non-empty value!"
+    exit 1
+fi
+echo -e "Your CLOUD_ID is set to $CLOUD_ID\n"
 echo "Enter you Elastic Cloud 'elastic' user password and then press [ENTER]"
 read CLOUD_AUTH
-echo -e "Your elastic password is set to $CLOUD_AUTH\n\n"
+if [ -z "$CLOUD_AUTH" ]; then
+    echo "Error: CLOUD_AUTH must be set to a non-empty value!"
+    exit 1
+fi
+echo -e "Your 'elastic' user password is set to $CLOUD_AUTH\n"
 echo "Ready to Install? [y|n]"
 read CONTINUE
 case "$CONTINUE" in
@@ -20,62 +30,59 @@ case "$CONTINUE" in
     *) echo "Installation aborted";exit;;
 esac
 
-# Download Elastic yum repo configuration.
-echo -e "\n\nDownloading Elastic yum repo configuration from github."
-sudo curl -O $CONFIG_REPOSITORY_URL/elastic-7.x.repo > /home/centos/elastic-7.x.repo
-sudo mv -v elastic-7.x.repo /etc/yum.repos.d/
-echo -e "\n\nInstalling filebeat, packetbeat, metricbeat, and auditbeat rpms"
-sudo yum install filebeat packetbeat metricbeat auditbeat -y
-echo -e "\n\nDownloading beats configuration files \n\n"
-sudo curl -O $CONFIG_REPOSITORY_URL/auditd-attack.rules.conf 
-sudo mv -v ./auditd-attack.rules.conf /etc/auditbeat/audit.rules.d/auditd-attack.rules.conf
-sudo curl -O $CONFIG_REPOSITORY_URL/auditbeat.yml > ./auditbeat.yml
-sudo mv -v ./auditbeat.yml /etc/auditbeat/auditbeat.yml
-sudo chown root /etc/auditbeat/auditbeat.yml
-sudo chmod go-w /etc/auditbeat/auditbeat.yml
-sudo curl -O $CONFIG_REPOSITORY_URL/filebeat.yml > ./filebeat.yml
-sudo mv -v ./filebeat.yml /etc/filebeat/filebeat.yml
-sudo chown root /etc/filebeat/filebeat.yml
-sudo chmod go-w /etc/filebeat/filebeat.yml
-sudo curl -O $CONFIG_REPOSITORY_URL/metricbeat.yml > ./metricbeat.yml
-sudo mv -v ./metricbeat.yml /etc/metricbeat/metricbeat.yml
-sudo chown root /etc/metricbeat/metricbeat.yml
-sudo chmod go-w /etc/metricbeat/metricbeat.yml
-sudo curl -O $CONFIG_REPOSITORY_URL/packetbeat.yml > ./packetbeat.yml
-sudo mv -v ./packetbeat.yml  /etc/packetbeat/packetbeat.yml
-sudo chown root  /etc/packetbeat/packetbeat.yml
-sudo chmod go-w /etc/packetbeat/packetbeat.yml
-echo -e "\n\nSetting up keystore with Elastic Cloud credentials"
-sudo auditbeat keystore create --force
-sudo filebeat keystore create --force
-sudo packetbeat keystore create --force
-sudo metricbeat keystore create --force
-echo $CLOUD_ID|sudo auditbeat keystore add CLOUD_ID --stdin --force
-echo $CLOUD_AUTH|sudo auditbeat keystore add --stdin CLOUD_AUTH --force
-echo $CLOUD_ID|sudo filebeat keystore add CLOUD_ID --stdin --force
-echo $CLOUD_AUTH|sudo filebeat keystore add --stdin CLOUD_AUTH --force
-echo $CLOUD_ID|sudo packetbeat keystore add CLOUD_ID --stdin --force
-echo $CLOUD_AUTH|sudo packetbeat keystore add --stdin CLOUD_AUTH --force
-echo $CLOUD_ID|sudo metricbeat keystore add CLOUD_ID --stdin --force
-echo $CLOUD_AUTH|sudo metricbeat keystore add --stdin CLOUD_AUTH --force
-echo -e "\n\nStopping auditd deamon"
-sudo service auditd stop
-echo -e "\n\nSetting up auditbeat"
-sudo auditbeat setup
-sudo systemctl start auditbeat
-echo -e "\n\nSetting up filebeat"
-sudo filebeat modules enable system
-sudo filebeat setup
-sudo systemctl start filebeat
-echo -e "\n\nSetting up packetbeat"
-sudo packetbeat setup
-sudo systemctl start packetbeat
-echo -e "\n\nSetting up metricbeat"
-sudo metricbeat setup
-sudo systemctl start metricbeat
-echo -e "\n\nTesting beats output"
-sudo auditbeat test output
-sudo metricbeat test output
-sudo filebeat test output
-sudo packetbeat test output
+echo -e "\nDownloading Elastic yum repo configuration..."
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+wget -q -N $CONFIG_REPOSITORY_URL/elastic-7.x.repo -P /etc/yum.repos.d/
+
+function install_beat() {
+    BEAT_NAME=$1
+    if [ $BEAT_NAME == "heartbeat" ]; then
+        BEAT_PKG_NAME="heartbeat-elastic" 
+    else
+        BEAT_PKG_NAME=$BEAT_NAME
+    fi 
+
+    yum -q list installed $BEAT_NAME &> /dev/null
+    if [ $? > 0 ]; then 
+        echo "$BEAT_NAME was previously installed. Uninstalling first..."
+        yum -y -q remove $BEAT_PKG_NAME 2>1 /dev/null
+        rm -Rf /etc/$BEAT_NAME /var/lib/$BEAT_NAME /var/log/$BEAT_NAME
+    fi
+
+    echo -e "\n*** Installing $BEAT_NAME ****";
+    yum -y install $BEAT_PKG_NAME
+    echo "Downloading $BEAT_NAME config file..."
+    wget -q -N $CONFIG_REPOSITORY_URL/$BEAT_NAME.yml -P /etc/$BEAT_NAME
+    chmod go-w /etc/$BEAT_NAME/$BEAT_NAME.yml
+    echo "Setting up $BEAT_NAME keystore with Elastic Cloud credentials"
+    $BEAT_NAME keystore create --force
+    echo $CLOUD_ID | $BEAT_NAME keystore add CLOUD_ID --stdin --force
+    echo $CLOUD_AUTH | $BEAT_NAME keystore add --stdin CLOUD_AUTH --force
+    
+    case $BEAT_NAME in
+        auditbeat)
+            wget -q -N $CONFIG_REPOSITORY_URL/auditd-attack.rules.conf -P /etc/auditbeat/audit.rules.d
+            echo "Stopping auditd deamon"
+            service auditd stop > /dev/null
+            chkconfig auditd off
+            ;;
+        filebeat)
+            $beatname modules enable system
+            ;;
+    esac
+
+    echo "Setting up $BEAT_NAME"
+    $BEAT_NAME setup
+    systemctl start $BEAT_NAME
+    chkconfig --add $BEAT_NAME
+    $BEAT_NAME test output
+    echo -e "$BEAT_NAME setup complete"
+}
+
+install_beat "auditbeat"
+install_beat "packetbeat"
+install_beat "metricbeat"
+install_beat "filebeat"
+install_beat "heartbeat"
+
 echo -e "\n\nSetup complete"
