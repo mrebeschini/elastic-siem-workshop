833d24f4b52af58f14ecfd6b3bc81d2f7256a48e
Matteo Rebeschini <matteo@elastic.co>
2019-11-27 10:12:41 -0700

    Update beats-install.ps1

--- beats-install.ps1
+++ beats-install.ps1
@@ -1,4 +1,4 @@
-$StackVersion = '7.4.1'
+$StackVersion = '7.4.2'
 $InstallFolder = "C:\Program Files\Elastic"
 $ConfigRepositoryURL = "https://raw.githubusercontent.com/mrebeschini/elastic-siem-workshop/master/"
 
